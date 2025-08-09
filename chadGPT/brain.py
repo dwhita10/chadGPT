16c3c853ea1e41af9e6108c449cd51374158444c
Dallin Whitaker <dallin.whitaker@iff.com>
2025-08-09 12:29:37 -0400

    initial data model done; orchestrator abstractclass done

---
+++ chadGPT/brain.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+from chadGPT.data_models import LLMRequest
+
+
+
+
---
+++ chadGPT/data_models.py
@@ -0,0 +1,49 @@
+from typing import Optional, Callable
+
+from datetime import datetime
+from pydantic import BaseModel
+
+
+# brain related data models (LLM)
+class LLMRequest(BaseModel):
+    prompt: str
+    background: Optional[str]
+    context: Optional[list[BaseModel | str]]
+    expected_format: Optional[BaseModel]
+
+
+# giga (orchestrator) related data models
+class Task(BaseModel):
+    func: Callable
+    args: tuple
+
+
+class Job(BaseModel):
+    schedule: str # cron schedule syntax see https://crontab.guru/
+    tasks: list[Task] # each sequential task should be unloaded into the next
+
+
+# trade related data models
+class Rule(BaseModel):
+    stop_loss: Optional[float]
+    take_profit: Optional[float]
+    sell_date: Optional[datetime]
+
+
+class Trade(BaseModel):
+    symbol: str
+    amount: float
+    trade_time: Optional[datetime]
+    rules: list[Rule]
+
+
+class Position(BaseModel):
+    symbol: str
+    shares: float
+    value: Optional[float]
+
+
+class Portfolio(BaseModel):
+    positions: list[Position]
+    cash: float
+
